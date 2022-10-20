76e1d4c3725531b0d8630862e0742aed060e6803
maxiibrunaa123 <maxiibrunaa123@gmail.com>
2022-10-20 03:09:39 -0300

    NewCommit

---
+++ src/com/company/Coche.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Coche {
+    String name = "Nombre de coche";
+
+    public Coche() {
+    }
+
+    public Coche(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
